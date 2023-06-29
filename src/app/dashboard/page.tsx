26491df0e283de0c27ae81775ddeb4c27584bca4
durimfe <durim.dev@gmail.com>
2023-06-29 15:27:24 +0200

    added dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const page: FC = () => {
+  return (
+    <div className="h-screen flex items-center justify-center bg-brand-background">
+      <p>Dashboard</p>
+    </div>
+  );
+};
+
+export default page;
