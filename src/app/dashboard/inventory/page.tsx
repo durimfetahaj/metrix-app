bc9c63c9ba499375543db273faf91bf5a7623f0c
durimfe <durim.dev@gmail.com>
2023-07-04 16:49:32 +0200

    removed inventory function from components and returned inside the page all components

--- src/app/dashboard/inventory/page.tsx
+++ src/app/dashboard/inventory/page.tsx
@@ -1,8 +1,20 @@
+import { Icons } from "@/components/Icons";
+import PageHead from "@/components/PageHead";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import { FC } from "react";
-import Inventory from "@/components/Intentory";
+import React from "react";
 
 const page: FC = () => {
-  return <Inventory />;
+  return (
+    <PageHead text="Inventory">
+      <Link href="/dashboard/inventory/new">
+        <Button>
+          <Icons.plus /> Add a New Product
+        </Button>
+      </Link>
+    </PageHead>
+  );
 };
 
 export default page;
