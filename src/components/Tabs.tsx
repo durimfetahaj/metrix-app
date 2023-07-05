fafd07b397fe9cfa27555256f872688ac862af5e
durimfe <durim.dev@gmail.com>
2023-07-05 09:23:30 +0200

    installed Tabs

---
+++ src/components/Tabs.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React, { useState } from "react";
+import {
+  Tabs as ShadTabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs";
+
+type Tab = {
+  value: string;
+  label: string;
+  content: React.ReactNode;
+};
+
+type TabsProps = {
+  tabs: Tab[];
+  defaultValue: string;
+};
+
+const Tabs: React.FC<TabsProps> = ({ defaultValue, tabs }) => {
+  const [activeTab, setActiveTab] = useState("");
+
+  const handleTabChange = (value: string) => {
+    setActiveTab(value);
+  };
+
+  return (
+    <ShadTabs defaultValue={defaultValue}>
+      <TabsList>
+        {tabs.map((tab: Tab) => (
+          <TabsTrigger key={tab.value} value={tab.value}>
+            {tab.label}
+          </TabsTrigger>
+        ))}
+      </TabsList>
+
+      {tabs.map((tab: Tab) => (
+        <TabsContent key={tab.value} value={tab.value}>
+          {tab.content}
+        </TabsContent>
+      ))}
+    </ShadTabs>
+  );
+};
+
+export default Tabs;
