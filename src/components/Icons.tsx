6fff8266a4cfffa722f253b2e173be5122643b11
durimfe <durim.dev@gmail.com>
2023-07-06 14:14:23 +0200

    created UploadImage component

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -11,6 +11,7 @@ import {
   ArrowLeftFromLine,
   Loader2,
   Plus,
+  UploadCloud,
 } from "lucide-react";
 
 export const Icons = {
@@ -64,4 +65,140 @@ export const Icons = {
     </svg>
   ),
   plus: (props: LucideProps) => <Plus {...props} className="mr-1" />,
+  uploadCloud: (props: LucideProps) => (
+    <svg
+      {...props}
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="32" height="32" rx="8" fill="#FFF2E2" />
+      <g clipPath="url(#clip0_713_8260)">
+        <path
+          d="M19.3337 19.3333L16.0003 16L12.667 19.3333"
+          stroke="#1C1D22"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M16 16V23.5"
+          stroke="#1C1D22"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M22.9919 21.3249C23.8047 20.8818 24.4467 20.1806 24.8168 19.3321C25.1868 18.4835 25.2637 17.5359 25.0354 16.6388C24.807 15.7417 24.2865 14.9462 23.5558 14.3778C22.8251 13.8094 21.9259 13.5005 21.0002 13.4999H19.9502C19.698 12.5243 19.2278 11.6185 18.5752 10.8507C17.9225 10.0829 17.1042 9.47311 16.182 9.06708C15.2597 8.66104 14.2573 8.46937 13.2503 8.50647C12.2433 8.54358 11.2578 8.80849 10.3679 9.28129C9.47795 9.7541 8.7068 10.4225 8.1124 11.2362C7.51799 12.05 7.11579 12.9879 6.93603 13.9794C6.75627 14.9709 6.80363 15.9903 7.07456 16.961C7.34548 17.9316 7.83291 18.8281 8.50021 19.5832"
+          stroke="#1C1D22"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M19.3337 19.3333L16.0003 16L12.667 19.3333"
+          stroke="#1C1D22"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_713_8260">
+          <rect
+            width="20"
+            height="20"
+            fill="white"
+            transform="translate(6 6)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  ),
+  remove: (props: LucideProps) => (
+    <svg
+      {...props}
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="32" height="32" rx="8" fill="#FFF2E2" />
+      <path
+        d="M22.1042 13.8906C22.1042 13.8906 21.6517 19.5031 21.3892 21.8673C21.2642 22.9965 20.5667 23.6581 19.4242 23.679C17.25 23.7181 15.0733 23.7206 12.9 23.6748C11.8008 23.6523 11.115 22.9823 10.9925 21.8731C10.7283 19.4881 10.2783 13.8906 10.2783 13.8906"
+        stroke="#130F26"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M23.2567 11.1999H9.125"
+        stroke="#130F26"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M20.5335 11.2C19.8793 11.2 19.316 10.7375 19.1877 10.0966L18.9852 9.08331C18.8602 8.61581 18.4368 8.29248 17.9543 8.29248H14.4268C13.9443 8.29248 13.521 8.61581 13.396 9.08331L13.1935 10.0966C13.0652 10.7375 12.5018 11.2 11.8477 11.2"
+        stroke="#130F26"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  ),
+  uploadCloudBlue: (props: LucideProps) => (
+    <svg
+      {...props}
+      width="17"
+      height="16"
+      viewBox="0 0 17 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clipPath="url(#clip0_713_11008)">
+        <path
+          d="M11.1663 10.6667L8.49967 8L5.83301 10.6667"
+          stroke="#5570F1"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M8.5 8V14"
+          stroke="#5570F1"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M14.0933 12.2598C14.7435 11.9053 15.2572 11.3444 15.5532 10.6656C15.8493 9.98674 15.9108 9.22865 15.7281 8.51095C15.5454 7.79326 15.129 7.15683 14.5444 6.70212C13.9599 6.24741 13.2406 6.00032 12.5 5.99983H11.66C11.4582 5.21932 11.0821 4.49472 10.5599 3.88049C10.0378 3.26626 9.3832 2.77839 8.64537 2.45356C7.90754 2.12874 7.10567 1.9754 6.30005 2.00508C5.49443 2.03476 4.70602 2.24669 3.99409 2.62494C3.28216 3.00318 2.66525 3.5379 2.18972 4.18888C1.7142 4.83987 1.39243 5.59018 1.24863 6.38342C1.10482 7.17666 1.14271 7.99218 1.35945 8.76867C1.57619 9.54515 1.96613 10.2624 2.49997 10.8665"
+          stroke="#5570F1"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M11.1663 10.6667L8.49967 8L5.83301 10.6667"
+          stroke="#5570F1"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_713_11008">
+          <rect
+            width="16"
+            height="16"
+            fill="white"
+            transform="translate(0.5)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  ),
 };
---
+++ src/components/UploadImage.tsx
@@ -0,0 +1,81 @@
+import { FC, HTMLAttributes, useRef } from "react";
+import { VariantProps, cva } from "class-variance-authority";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import { Icons } from "./Icons";
+
+const uploadImageVariants = cva(
+  "relative rounded-xl border-solid border-gray-200 bg-white bg-brand-background border",
+  {
+    variants: {
+      size: {
+        default: "h-40 w-44 px-4 py-7",
+        lg: "h-96 w-96  px-4 py-10",
+      },
+    },
+    defaultVariants: {
+      size: "default",
+    },
+  }
+);
+
+interface UploadImageProps
+  extends HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof uploadImageVariants> {
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const UploadImage: FC<UploadImageProps> = ({
+  className,
+  size,
+  onChange,
+  ...props
+}) => {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleImageUpload = () => {
+    fileInputRef.current?.click();
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(event);
+  };
+
+  return (
+    <div className={cn(uploadImageVariants({ size, className }))} {...props}>
+      <div
+        className="h-full w-full cursor-pointer flex flex-col items-center justify-center"
+        onClick={handleImageUpload}
+      >
+        <Image
+          height={60}
+          width={60}
+          src="/images/upload-img-fallback.png"
+          alt="upload-img-fallback"
+          className={size === "lg" ? "mb-10" : "mb-3"}
+        />
+        <div className="flex gap-3">
+          <Icons.uploadCloudBlue />
+          <p className="text-sm text-brand-primary-100 mb-5">Upload Image</p>
+        </div>
+        {size === "lg" && (
+          <p className="text-brand-black-30 text-sm text-center">
+            Upload a cover image for your product. File Format {""}
+            <span className="text-brand-black-90">jpeg, png</span> {""}
+            Recommened Size{" "}
+            <span className="text-brand-black-90">600x600 (1:1)</span>
+          </p>
+        )}
+        <input
+          ref={fileInputRef}
+          type="file"
+          accept="image/*"
+          className="invisible"
+          onChange={handleFileChange}
+        />
+      </div>
+    </div>
+  );
+};
+
+export { UploadImage, uploadImageVariants };
