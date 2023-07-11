80333a9d6b3200c590b905b554689ca0604bbb6c
durimfe <durim.dev@gmail.com>
2023-07-11 15:29:19 +0200

    added return

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,7 +1,21 @@
+"use client";
+
 import Header from "@/components/Header";
 import Sidebar from "@/components/Sidebar";
+import { app } from "@/firebase/firebaseConfig";
+import { useUserStore } from "@/store/useUser";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
 
 function DashboardLayout({ children }: { children: React.ReactNode }) {
+  const { setUser, user } = useUserStore();
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(getAuth(app), (user) => {
+      setUser(user);
+    });
+    return () => unsubscribe();
+  }, []);
+
   return (
     <div className="h-screen flex overflow-y-hidden">
       <Sidebar />
