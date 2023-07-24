d5d9006e5766774e1beb8642e83f5adeeb2dbdeb
durimfe <durim.dev@gmail.com>
2023-07-24 14:41:07 +0200

    removed api and used firebase sdk

--- src/app/dashboard/inventory/page.tsx
+++ src/app/dashboard/inventory/page.tsx
@@ -4,12 +4,12 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import React from "react";
 import Products from "./products";
+import { DocumentData, collection, getDocs } from "firebase/firestore";
+import { db } from "@/firebase/firebaseConfig";
 
 export default async function InventoryPage() {
-  const res = await fetch("/api/inventory", {
-    cache: "no-cache",
-  });
-  const products = await res.json();
+  const querySnapshot = await getDocs(collection(db, "products"));
+  const products: DocumentData[] = querySnapshot.docs.map((doc) => doc.data());
 
   return (
     <>
