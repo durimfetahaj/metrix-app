322c8d1e2b98fc656c2f048f3156aa259029f993
durimfe <durim.dev@gmail.com>
2023-08-01 13:28:04 +0200

    refactored summarycard

--- src/components/SummaryCard.tsx
+++ src/components/SummaryCard.tsx
@@ -4,27 +4,17 @@ import React from "react";
 type Props = {
   data: SummaryCard[];
   icon?: JSX.Element;
-  id: string;
-  isClicked: string;
-  setIsClicked: React.Dispatch<React.SetStateAction<string>>;
 };
 
-function SummaryCard({ data, icon, id, isClicked, setIsClicked }: Props) {
+function SummaryCard({ data, icon }: Props) {
   return (
-    <div
-      className={`flex flex-col px-4 py-3 w-full gap-5 cursor-pointer ${
-        isClicked === id
-          ? "bg-brand-primary-100 text-white "
-          : "bg-white text-black"
-      } rounded-xl`}
-      onClick={() => setIsClicked(id)}
-    >
+    <div className={`flex flex-col px-4 py-3 w-full gap-5 bg-white rounded-xl`}>
       {icon && icon}
       <div className="flex justify-between">
         {data?.map(({ title, value }) => {
           return (
             <div className="flex flex-col gap-2" key={title}>
-              <p>{title}</p>
+              <p className="text-brand-black-30 text-sm">{title}</p>
               <p>{value}</p>
             </div>
           );
