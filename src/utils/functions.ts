10342b8d5e3d43cf23286c1381045e00d6aeee63
durimfe <durim.dev@gmail.com>
2023-08-01 13:26:36 +0200

    added products utils functions

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,3 +1,5 @@
+import { DocumentData } from "firebase/firestore";
+
 export function getTitle(pathName: string) {
   let trimmedWord;
 
@@ -13,3 +15,48 @@ export function getTitle(pathName: string) {
 
   return trimmedWord;
 }
+
+export const getLowStockProducts = (products: DocumentData[] | null) => {
+  const threshold = 10; // Define your low stock threshold
+
+  if (!products) {
+    return [];
+  }
+
+  const lowStockProducts = products.filter((product) => {
+    const stockQuantity = product.stock || 0;
+    return stockQuantity <= threshold;
+  });
+
+  return lowStockProducts;
+};
+
+export const getExpiredProducts = (products: DocumentData[] | null) => {
+  if (!products) {
+    return [];
+  }
+
+  const currentDate = new Date();
+
+  const expiredProducts = products.filter((product) => {
+    const expiryDate = product.expiryDate?.toDate() || null;
+    return expiryDate && expiryDate < currentDate;
+  });
+
+  return expiredProducts;
+};
+
+export const getActiveProducts = (products: any[]) => {
+  const today = new Date();
+  const thirtyDaysAgo = new Date(today);
+  thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
+
+  const activeProducts = products.filter((product) => {
+    const lastTimeSold = product?.lastSoldTimestamp
+      ? product.lastSoldTimestamp.toDate()
+      : "N/A";
+    return new Date(lastTimeSold) >= thirtyDaysAgo;
+  });
+
+  return activeProducts;
+};
