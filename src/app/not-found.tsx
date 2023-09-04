3edc5bd04b324d463a06df333882f495f5daed16
durimfe <durim.dev@gmail.com>
2023-09-04 11:19:38 +0200

    changed not found style and added search page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -11,14 +11,6 @@ const page: FC = () => {
     <div className="flex flex-col items-center justify-center h-screen bg-brand-background">
       <h1 className="text-4xl font-bold mb-4">404</h1>
       <p className="text-lg text-gray-600 mb-8">Page not found</p>
-      <Link href="/dashboard">
-        <Button
-          className="bg-brand-primary-100 hover:bg-brand-primary-80 rounded-xl"
-          size="lg"
-        >
-          Go Home
-        </Button>
-      </Link>
     </div>
   );
 };
