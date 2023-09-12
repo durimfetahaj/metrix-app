631a89ff8115087e6c9d22490ae1e4523f6a0128
durimfe <durim.dev@gmail.com>
2023-09-12 15:19:39 +0200

    removed bg red

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,7 +2,7 @@ import Sidebar from "@/components/Sidebar";
 
 function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex bg-red-500 h-screen">
+    <div className="flex h-screen">
       <Sidebar />
       <div className="p-5 max-w-xs md:max-w-7xl w-full overflow-scroll md:overflow-hidden">
         {children}
